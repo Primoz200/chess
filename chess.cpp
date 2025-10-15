04f8ea944081a339151f5ffc037b7c9fcf840505
Primoz <primoz.plevel@gmail.com>
2025-10-15 18:49:37 +0200

    Added Move class, improved pawn function

--- chess.cpp
+++ chess.cpp
@@ -54,64 +54,126 @@ void printBoard(vector<vector<int>> &v) {
             // if (v[i][j] < 10) { cout << " "; }
         }
     cout << endl;
-}
-}
-
-array<int, 4> enumerateMove(string move) { //vrne indekse v tabeli. pozcija 0 stolpec,1 vrstica (ker e4 in ne 4e) za zacetni square, 2,3 za koncni
-    return {move[0] - 'a', 7 - (move[1] - '1'), move[2] - 'a', 7 - (move[3] - '1')};
+    }
 }
 
-bool kings(vector<vector<int>> &board, array<int, 4> move, bool color){return false;}
+class Move {
+    public:
+        int fromX;
+        int fromY;
+        int toX;
+        int toY;
 
-bool pawns(vector<vector<int>> &board, array<int, 4> move, bool color){  //returns true if move is valid and moves piece if it is
-    // for(int i: move) {
-    //     cout << i << " ";
-    // }
-    if(!color) {
-        if(move[3] - move[1] >= 3) {
+        Move(int x1, int y1, int x2, int y2) {
+            fromX = x1;
+            fromY = y1;
+            toX = x2;
+            toY = y2;
+        }
+
+};
+
+// array<int, 4> enumerateMove(string move) { //vrne indekse v tabeli. pozcija 0 stolpec,1 vrstica (ker e4 in ne 4e) za zacetni square, 2,3 za koncni
+//     return {move[0] - 'a', 7 - (move[1] - '1'), move[2] - 'a', 7 - (move[3] - '1')};
+// }
+
+Move string2Move(string moveS) {
+    Move move(moveS[0] - 'a', 7 - (moveS[1] - '1'), moveS[2] - 'a', 7 - (moveS[3] - '1'));
+    return move;
+}
+
+void makeMove(vector<vector<int>> &board, Move &move) {
+    board[move.toY][move.toX] = board[move.fromY][move.fromX];
+    board[move.fromY][move.fromX] = 0;
+}
+
+bool kings(vector<vector<int>> &board, Move move, bool color){return false;}
+
+bool pawns(vector<vector<int>> &board, Move move, bool color){  //returns true if move is valid and moves piece if it is
+    if(color) {     //white pawns
+        if(move.fromY - move.toY > 2) {
             return false;
-        }else if(move[3] - move[1] == 2){       //move by 2
-            if(move[1] != 1) return false;   //not seventh rank
-            else if(move[0] != move[2]) return false;   //not same column   
-            else if(board[move[3]][move[2]] != 0 || board[move[3]+1][move[2]] != 0) {return false; } //check if space is empty
+        }else if(move.fromY - move.toY == 2){       //move by 2
+            if(move.fromY != 6) return false;   //not seventh rank
+            else if(move.fromX != move.toX) return false;   //not same column   
+            else if(board[move.toY][move.toX] != 0 || board[move.toY-1][move.toX] != 0) return false; //check if space is empty
             else {
-                board[move[3]][move[2]] = board[move[1]][move[0]];
-                board[move[1]][move[0]] = 0;
+                makeMove(board, move);
                 return true;
             }
-        }
+        }else if(move.fromY - move.toY == 1){           //move by 1
+            if(move.fromX == move.toX){                 //same file
+                if(board[move.toY][move.toX] != 0) return false;
+                else {
+                    makeMove(board, move);
+                    return true;
+                }
+            }else if(abs(move.fromX - move.toX) != 1) return false;
+            else if(board[move.toY][move.toX] >= 7) {     
+                makeMove(board, move);               
+                return true;
+            }
+        } 
+    }
+    else {          //black pawns
+        if(move.toY - move.fromY > 2) {
+            return false;
+        }else if(move.toY - move.fromY == 2){       //move by 2
+            if(move.fromY != 1) return false;   //not seventh rank
+            else if(move.fromX != move.toX) return false;   //not same column   
+            else if(board[move.toY][move.toX] != 0 || board[move.toY-1][move.toX] != 0) {return false; } //check if space is empty
+            else {
+                makeMove(board, move);
+                return true;
+            }
+        }else if(move.toY - move.fromY == 1){           //move by 1
+            if(move.fromX == move.toX){                 //same file
+                if(board[move.toY][move.toX] != 0) return false;
+                else {
+                    makeMove(board, move);
+                    return true;
+                }
+            }else if(abs(move.fromX - move.toX) != 1) return false;
+            else if(board[move.toY][move.toX] <= 6) {     //if smaller than 6 => is a white piece
+                makeMove(board, move);               
+                return true;
+            }
+        } 
     }
     
     return false;
 }
 
-bool rooks(vector<vector<int>> &board, array<int, 4> move, bool color){return false;}
-bool knights(vector<vector<int>> &board, array<int, 4> move, bool color){return false;}
-bool bishops(vector<vector<int>> &board, array<int, 4> move, bool color) {return false;}
-bool queens(vector<vector<int>> &board, array<int, 4> move, bool color) {return false;}
+bool rooks(vector<vector<int>> &board, Move move, bool color){
+    return false;
+}
+bool knights(vector<vector<int>> &board, Move move, bool color){return false;}
+bool bishops(vector<vector<int>> &board, Move move, bool color) {return false;}
+bool queens(vector<vector<int>> &board, Move move, bool color) {return false;}
 
-bool makeCurMove(vector<vector<int>> &board, array<int, 4> enumMove, bool color) { // returns true if move was made
-    int figura = board[enumMove[1]][enumMove[0]];
+bool evalCurMove(vector<vector<int>> &board, Move move, bool color) { // returns true if move was made
+    int figura = board[move.fromY][move.fromX];
     bool valid = false;
     switch (figura % 6) {
         case 0:
-            valid = kings(board, enumMove, color);
+            valid = kings(board, move, color);
             break;
         case 1:
-            valid = pawns(board, enumMove, color);
+            valid = pawns(board, move, color);
             break;
         case 2:
-            valid = rooks(board, enumMove, color);
+            valid = rooks(board, move, color);
             break;
         case 3:
-            valid = knights(board, enumMove, color);
+            valid = knights(board, move, color);
             break;
         case 4:
-            valid = bishops(board, enumMove, color);
+            valid = bishops(board, move, color);
             break;
         case 5:
-            valid = queens(board, enumMove, color);
+            valid = queens(board, move, color);
     }
+
     return valid;
 }
 
@@ -137,7 +199,16 @@ int main() {
         }
 
         cin >> curMove;
-        makeCurMove(board, enumerateMove(curMove), nOfMoves % 2 == 1? true : false);
+        while(!evalCurMove(board, string2Move(curMove), nOfMoves % 2 == 1? true : false)){
+            cout << "invalid move. "; 
+            if(nOfMoves % 2 == 1) {
+                cout << "White to move: \n";
+            }
+            else{
+                cout << "Black to move: \n";
+            }
+            cin >> curMove;
+        }
         
         nOfMoves++;
     }
