a8b02c3ca8d4689c14eb6c3792a61094c264eeed
Primoz200 <primoz.plevel@gmail.com>
2025-11-28 21:10:53 +0100

    tables for piece worth, moving funcitons in separate file

--- bot.cpp
+++ bot.cpp
@@ -14,13 +14,47 @@
 using namespace std;
 
 map<int, int> pieceWorth = {
-    {1, 1},
-    {2, 5},
-    {3, 3},
-    {4, 3},
-    {5, 9},
-    {6, 5}
+    {1, 10},
+    {2, 50},
+    {3, 30},
+    {4, 30},
+    {5, 90},
+    {6, 40}
 };
+
+vector<vector<int>> pawnValue = {
+    {15, 15, 15, 15, 15, 15, 15, 15},
+    {14, 14, 15, 15, 15, 15, 14, 14},
+    {12, 11, 11, 12, 12, 11, 11, 12},
+    {10, 10, 11, 13, 13, 11, 10, 10},
+    {10, 10, 11, 13, 13, 11, 10, 10},
+    {9, 10, 10, 12, 12, 10, 10, 9},
+    {9, 10, 10, 10, 10, 10, 10, 9},
+    {10, 10, 10, 10, 10, 10, 10, 10}
+};
+
+vector<vector<int>> knightValue = {
+    {24, 26, 28, 28, 28, 28, 26, 24},
+    {26, 28, 30, 30, 30, 30, 28, 26},
+    {26, 30, 32, 32, 32, 32, 30, 26},
+    {28, 30, 32, 33, 33, 32, 30, 28},
+    {28, 30, 32, 33, 33, 32, 30, 28},
+    {26, 30, 32, 32, 32, 32, 30, 26},
+    {26, 28, 30, 30, 30, 30, 28, 26},
+    {24, 26, 28, 28, 28, 28, 26, 24}
+};
+
+vector<vector<int>> kingValueMiddleGame = {
+    {30, 30, 30, 30, 30, 30, 30, 30},
+    {30, 30, 30, 30, 30, 30, 30, 30},
+    {30, 30, 30, 30, 30, 30, 30, 30},
+    {30, 30, 30, 30, 30, 30, 30, 30},
+    {35, 35, 35, 35, 35, 35, 35, 35},
+    {38, 38, 38, 38, 38, 38, 38, 38},
+    {43, 43, 41, 41, 41, 41, 43, 43},
+    {48, 48, 48, 44, 44, 45, 48, 48}
+};
+
 
 void shuffleVector(vector<Move>& v){
     std::random_device rd;      // non-deterministic seed
@@ -47,18 +81,44 @@ void forceMoveH(vector<vector<int>> &board, Move move, int oldPiece=0){         
 
 int evaluate(vector<vector<int>> &board, bool isWhite, int gamestate){
     int eval = 0;
+    int pieceCount=0;
+    pair<int,int> whiteKingPosition = {-1, -1};
+    pair<int,int> blackKingPosition = {-1, -1};
 
-    if(gamestate == DRAW ||gamestate == STALEMATE) return 0;
-    if(gamestate == WHITE_WIN) return 1000;
-    if(gamestate == BLACK_WIN) return -1000;
+    if(gamestate == DRAW || gamestate == STALEMATE) return 0;
+    if(gamestate == WHITE_WIN) return 10000;
+    if(gamestate == BLACK_WIN) return -10000;
 
     for(int x = 0; x < 8; x++){
         for(int y = 0; y < 8; y++){
             int piece = board[y][x];
             if(piece == 0) continue;
-            eval += colorMultiplier(piece) * pieceWorth[piece % 6];
+            pieceCount++;
+            if(piece % 6 == 0) {
+                if(piece == 6) {
+                    whiteKingPosition.first = x;
+                    whiteKingPosition.second = y;
+                }else{
+                    blackKingPosition.first = x;
+                    blackKingPosition.second = y;
+                }
+            }
+            else if(piece%6 == 1){
+                int pawnV = isWhite ? pawnValue[y][x] : pawnValue[7-y][x];
+                eval += colorMultiplier(piece) * pawnV;
+            }
+            else if(piece % 6 == 3){
+                eval += colorMultiplier(piece) * knightValue[x][y];
+            }
+            else{ eval += colorMultiplier(piece) * pieceWorth[piece % 6];}
+    
         }
     }
+
+    if(pieceCount > 10){        //arbitratry selected piece number for when it transitions into an endgame
+        eval+=kingValueMiddleGame[whiteKingPosition.second][whiteKingPosition.first];
+        eval+= -kingValueMiddleGame[7 - blackKingPosition.second][blackKingPosition.first];
+    }//else endgame table
 
     return eval;
 }
@@ -69,7 +129,7 @@ string getBotMove(vector<vector<int>> &board, vector<Move> &moves, bool isWhite,
     tempBoard = board;
     string s = "";
 
-    int maxEval = isWhite ? -1001 : 1001;
+    int maxEval = isWhite ? -10001 : 10001;
     int indMove = -1;
     for(int i = 0; i < moves.size(); i++){
         if(moves[i].isNull()) continue;
---
+++ functions.cpp
@@ -0,0 +1,2 @@
+//meant for functions used in bot.cpp and chess.cpp
+
